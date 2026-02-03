4a21810b497a919c1967765db2e90fc15f76a6d2
prince Raj <princeraj2722@gmail.com>
2026-02-03 23:32:11 +0530

    fixes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://127.0.0.1:8000/api/:path*',
+        },
+      ]
+    }
+    return [];
+  },
 };
 
 export default nextConfig;
