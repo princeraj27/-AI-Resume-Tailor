53f389be7f012f8438dc9eb15fc385d1e52de55b
prince Raj <princeraj2722@gmail.com>
2026-02-03 23:36:19 +0530

    fixes

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,6 +1,11 @@
 import os
 import sys
 
+# Serverless Environment Config
+# Vercel FS is read-only except for /tmp
+os.environ['HF_HOME'] = '/tmp/huggingface'
+os.environ['TRANSFORMERS_CACHE'] = '/tmp/huggingface'
+
 # Add the parent directory (backend) to sys.path so we can import main
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
